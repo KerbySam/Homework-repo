8406cdd3fd2d48401c4a9c315e3bd3f98775fed0
Sam <zayaza88@mail.ru>
2019-02-10 15:03:21 +0300

    add tasks

---
+++ Homework4/Homework4/Homework4/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Homework4
+//
+//  Created by sergey.lukin on 10/02/2019.
+//  Copyright © 2019 fynker.development. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
