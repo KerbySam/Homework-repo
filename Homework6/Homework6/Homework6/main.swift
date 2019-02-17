4330d1c7d827542eea9c938b86e805705cf76725
Kerby <zayaza88@mail.ru>
2019-02-18 00:39:40 +0300

    Done

---
+++ Homework6/Homework6/Homework6/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Homework6
+//
+//  Created by sergey.lukin on 17/02/2019.
+//  Copyright © 2019 fynker.development. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
