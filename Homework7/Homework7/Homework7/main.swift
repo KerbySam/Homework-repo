1e4f08891f1dd4dfe84cb81cddca72063b7a58ab
Kerby <zayaza88@mail.ru>
2019-02-20 22:09:10 +0300

    Create project

---
+++ Homework7/Homework7/Homework7/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Homework7
+//
+//  Created by sergey.lukin on 20/02/2019.
+//  Copyright © 2019 fynker.development. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
